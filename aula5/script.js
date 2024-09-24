4d55254248fba27dc0c1d3dfebd3a921a444190a
rafis-af <rafa.ferreira0301@gmail.com>
2024-09-24 17:04:32 -0300

    24/09

---
+++ aula5/script.js
@@ -0,0 +1,46 @@
+
+
+console.log('olá');
+
+//window.alert('oi');
+
+//let nome = prompt('Qual o seu nome?')
+
+//console.log(nome);
+
+//for(let i = 0; i < 100; i++){
+//    if(i % 2 === 1){
+//        console.log(i)
+//    }
+
+//}
+
+//for(let i = 0; i < 505; i++){
+//    if(i % 5 === 0){
+//        console.log(i)
+//    }
+
+
+//}
+
+
+//let entrada = prompt('Digite um número: ')
+//if(!isNaN(parseInt(entrada))){
+//    for(let i = entrada; i != -1; i--){
+//        console.log(i)
+//    }
+//}else{
+//    window.alert('Valor errado')
+
+//}
+
+let entrada = prompt('Digite um número: ')
+
+for(let i = entrada; i > 0; i * (entrada - 1)){
+
+        console.log(i)
+
+}
+
+
+
